10159153564d567da61a242486077d940a55c712
yaning <hhhhhsherlock@gmail.com>
2025-02-28 17:33:16 +0100

    use threshold for firing prevent overflow

--- threshold/Network.py
+++ threshold/Network.py
@@ -6,7 +6,7 @@ Created on Wed Dec 04
 @author: yaning
 """
 
-import with_learning.learning_NN.Receptors as Receptors
+import threshold.Receptors as Receptors
 import importlib
 import numpy as np
 import matplotlib.pyplot as plt
@@ -15,21 +15,24 @@ import random
 #--------------------------------Neuron--------------------------------------
 class Neuron:
     _Cm = 1
+    _threshold = -58
 
     def __init__(self, deltaTms, I, Vm, Name):
         self.deltaTms = deltaTms
         self.I = I
         self.Vm = Vm
-        self.Name = Name      
+        self.Name = Name  
+        self.fire_state = 0
+        self.fire_count = 0
         self.fire_tstep = []  
 
         self.incoming_synapses = []
         self.outgoing_synapses = []
         
-        # # for threshold we don't need ion channels
-        # self._sodium_channel = Receptors.Voltage_Sodium(self.Vm)
-        # self._potassium_channel = Receptors.Voltage_Potassium(self.Vm)
-        # self._leaky_channel = Receptors.Voltage_Leak(self.Vm)
+    
+        self._sodium_channel = Receptors.Voltage_Sodium(self.Vm)
+        self._potassium_channel = Receptors.Voltage_Potassium(self.Vm)
+        self._leaky_channel = Receptors.Voltage_Leak(self.Vm)
         
     @staticmethod
     def count_all_continuous_sequences(arr):
@@ -52,27 +55,74 @@ class Neuron:
     # voltage and ion channel currents update has to be here because it sums from all dendrites
     def update(self):
 
-        # Q: should i update voltage before the gP, do they make a difference?
-        # update the ion channel voltage
-        # self._sodium_channel.Vm = self.Vm
-        # self._potassium_channel.Vm = self.Vm
-        # self._leaky_channel.Vm = self.Vm
+        if self.fire_state == 0:
+            error_code = 0
+            # Q: should i update voltage before the gP, do they make a difference?
+            # update the ion channel voltage
+            self._sodium_channel.Vm = self.Vm
+            self._potassium_channel.Vm = self.Vm
+            self._leaky_channel.Vm = self.Vm
 
-        Ireceptors = 0
-        synapses = self.incoming_synapses
-        for synapse in synapses:
-            receptors = synapse.receptors
-            for receptor in receptors:
-                receptor.Vm = self.Vm
-                receptor.update_gP(synapse.state, self.deltaTms)
-                Ireceptors += receptor.current()
-                    
+
             
+            try:
+                # ion channel currents
+                self._sodium_channel.update_gP(self.deltaTms)
+                self._potassium_channel.update_gP(self.deltaTms)
+                
+                # update the receptors gP in neuron
+                # cannot do it in synapse because the amount of updating will depend on the connections 
+                # but it shouldn't 
+                Ireceptors = 0
+                synapses = self.incoming_synapses
+                for synapse in synapses:
+                    receptors = synapse.receptors
+                    for receptor in receptors:
+                        receptor.Vm = self.Vm
+                        receptor.update_gP(synapse.state, self.deltaTms)
+                        Ireceptors += receptor.current()
+                        
+                
+                
+                # get the currents
+                Ina = self._sodium_channel.current()
+                Ik = self._potassium_channel.current()
+                Ileak = self._leaky_channel.current()
 
-        # add the ion channel currents, what about the ion channel currents
-        self.I = Ireceptors
-        self.Vm += - self.deltaTms * self.I / self._Cm
+                # not caring the ligand gated receptors for now
 
+                currents = {
+                    "INa": Ina,
+                    "IK": Ik,
+                    "Ileak": Ileak,
+                    "IReceptor": Ireceptors
+                    # "Iampa": Iampa,
+                    # "Inmda": Inmda,
+                    # "Igaba": Igaba
+                }
+                # check for over or underflow
+                
+                for name, current in currents.items():
+                    if current > 1e10:
+                        raise OverflowError(f"Overflowed: {name} = {current}")
+            except OverflowError as m:
+                # print(f"error: {m}")
+                # print("this line runs")
+                error_code += 1
+                
+
+            # add the ion channel currents
+            self.I = Ina + Ik + Ileak + Ireceptors
+            ion_I = Ina + Ik + Ileak
+            self.Vm += - self.deltaTms * self.I / self._Cm
+
+        if self.Vm >= self.fire_threshold and self.fire_count <= 61:
+            self.fire_state = 1
+            self.fire_count += 1
+        
+        
+
+        return error_code, ion_I
 
     # when this neuron fires, send signal to the connected post synapses
     # this step is problematic because it only sends out signal when i am certain it fires 
@@ -132,68 +182,5 @@ class Synapse:
         self.receptors = []
         if args:
             self.receptors.extend(args)
-          
-
-
-# #--------------------------------Control (make connections)--------------------------------------
-# class Control:
-#     def __init__(self, deltaTms, Vm):
-#         self.all_synapses = []
-#         self.deltaTms = deltaTms
-#         # this voltage is the post synpase neuron voltage
-#         self.Vm = Vm
-
-#     # also let the neurons know their connections and keep record of all synapses so they can be updated easily
-#     def create_synapse(self, send_neuron, receive_neuron, type):
-        
-#         # create receptors accordingly
-#         if type == "AMPA":
-#             # temporal solution for weight randomise
-#             # Receptors.LigandGatedChannelFactory.set_params()
-#             ampa_receptor = Receptors.AMPA(0.00072, 1, -70, self.Vm, 0.9, 1, 1, 1, 12, 10, 20, 10, 35, 7, 0.7, "AMPA")
-#             synapse = Synapse(self.deltaTms, 0, send_neuron, receive_neuron, ampa_receptor)
-            
-#         elif type == "AMPA+NMDA":
-#             # Receptors.LigandGatedChannelFactory.set_params()
-#             ampa_receptor = Receptors.AMPA(0.00072, 1, -70, self.Vm, 0.9, 1, 1, 1, 12, 10, 20, 10, 35, 7, 0.7, "AMPA")
-#             nmda_receptor = Receptors.NMDA(0.0012, 1, -70, self.Vm, 0.9, 1, 1, 1, 12, 10, 20, 10, 15, 7, 0.7, "NMDA")
-#             synapse = Synapse(self.deltaTms, 0, send_neuron, receive_neuron, ampa_receptor, nmda_receptor)
-        
-#         elif type == "GABA":
-#             # Receptors.LigandGatedChannelFactory.set_params()
-#             # print(Receptors.LigandGatedChannelFactory.w_init_GABA)
-#             gaba_receptor = Receptors.GABA(0.0012, 1, -140, self.Vm, 0.9, 1, 1, 1, 12, 10, 20, 10, 20, 7, 0.7, "GABA")
-#             synapse = Synapse(self.deltaTms, 0, send_neuron, receive_neuron, gaba_receptor)
-
-#         send_neuron.outgoing_synapses.append(synapse)
-#         receive_neuron.incoming_synapses.append(synapse)
-
-#         self.all_synapses.append(synapse)
-
-#     def update_synapse_initial_values(self, infer_params):
-#         for synapse in self.all_synapses:
-#             for receptor in synapse.receptors:
-#                 receptor.e = infer_params["e_init"]
-#                 receptor.u_se = infer_params["u_se"]
-#                 receptor.g_decay = infer_params["g_decay_init"]
-#                 receptor.g_rise = infer_params["g_rise_init"]
-#                 receptor.w = infer_params["w_init"]
-#                 receptor.tau_rec = infer_params["tau_rec"]
-#                 receptor.tau_pre = infer_params["tau_pre"]
-#                 receptor.tau_post = infer_params["tau_post"]
-
-#                 if receptor.label == "GABA":
-#                     receptor.gMax = infer_params["gMax_GABA"]
-#                     receptor.tau_decay = infer_params["tau_decay_GABA"]
-#                     receptor.tau_rise = infer_params["tau_rise_GABA"]
-                
-#                 elif receptor.label == "NMDA":
-#                     receptor.tau_decay = infer_params["tau_decay_NMDA"]
-#                     receptor.tau_rise = infer_params["tau_rise_NMDA"]
-                
-#                 elif receptor.label == "AMPA":
-#                     receptor.tau_decay = infer_params["tau_decay_AMPA"]
-#                     receptor.tau_rise = infer_params["tau_rise_AMPA"]
-
         
             
